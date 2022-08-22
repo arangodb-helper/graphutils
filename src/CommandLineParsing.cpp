6e714cc6504d043850d10b7842f0a32f41d08fcf
Max Neunhoeffer <max@arangodb.com>
2022-08-22 22:26:45 +0200

    Add separate files.

---
+++ src/CommandLineParsing.cpp
@@ -0,0 +1,109 @@
+// CommandLineParsing.cpp - tools for command line parsing
+
+#include "CommandLineParsing.h"
+
+#include <iostream>
+
+int parseCommandLineArgs(
+    char const* usage, OptionConfig const& optionConfig, int argc, char* argv[],
+    std::unordered_map<std::string, std::vector<std::string>>& options,
+    std::vector<std::string>& args) {
+  // Build alias lookup:
+  std::unordered_map<std::string, std::string> aliases;
+  for (auto const& p : optionConfig) {
+    if (p.second.alias) {
+      aliases.emplace(p.second.alias.value(), p.first);
+    }
+  }
+
+  options.clear();
+  args.clear();
+  bool pastOptions = false;
+  for (size_t i = 1; i < argc; ++i) {
+    std::string a{argv[i]};
+    std::string b;
+    if (a[0] == '-' && a.size() > 0 && !pastOptions) {  // seems to be an option
+      if (a == "--") {
+        pastOptions = true;
+      } else {
+        // Recognize = sign in option:
+        auto pos = a.find('=');
+        bool advanced = false;
+        if (pos != std::string::npos) {
+          b = a.substr(pos + 1);
+          a = a.substr(0, pos);
+        } else {
+          if (i + 1 < argc) {
+            b = argv[i + 1];
+            ++i;
+            advanced = true;
+          }
+        }
+
+        // Use aliases:
+        auto it = aliases.find(a);
+        if (it != aliases.end()) {
+          a = it->second;
+        }
+
+        // Find option in config:
+        auto it2 = optionConfig.find(a);
+        if (it2 == optionConfig.end()) {
+          std::cerr << "Unknown option '" << a << "', giving up.\n"
+                    << usage << std::endl;
+          return 1;
+        }
+
+        // Check rules:
+        ArgType at = it2->second.argType;
+
+        // Check value:
+        if (at == Bool) {
+          if (b == "false" || b == "FALSE" || b == "False" || b == "No" ||
+              b == "NO" || b == "no" || b == "f" || b == "F" || b == "n" ||
+              b == "N") {
+            b = "false";
+          } else if (b == "true" || b == "TRUE" || b == "True" || b == "Yes" ||
+                     b == "YES" || b == "yes" || b == "t" || b == "T" ||
+                     b == "y" || b == "Y") {
+            b = "true";
+          } else {  // Do not accept this argument
+            b = "true";
+            if (advanced) {
+              --i;
+            }
+          }
+        }
+
+        // Now set value:
+        auto it3 = options.find(a);
+        if (it3 == options.end()) {
+          options.emplace(a, std::vector<std::string>{b});
+        } else {
+          if (at == ArgType::StringOnce || at == ArgType::Bool) {
+            std::cerr << "Option '" << a
+                      << "' must only occur once, giving up.\n"
+                      << usage << std::endl;
+            return 2;
+          }
+          it3->second.push_back(b);
+        }
+      }
+    } else {
+      args.push_back(a);
+    }
+  }
+
+  // Finally, set defaults:
+  for (auto const& p : optionConfig) {
+    if (p.second.defaultValue) {
+      auto it = options.find(p.first);
+      if (it == options.end()) {
+        options.emplace(
+            p.first, std::vector<std::string>{p.second.defaultValue.value()});
+      }
+    }
+  }
+  return 0;
+}
+
---
+++ src/CommandLineParsing.h
@@ -0,0 +1,34 @@
+// CommandLineParsing.h - tools to parse command lines
+
+#pragma once
+
+#include <optional>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+enum ArgType {
+  Bool = 0,
+  StringOnce = 3,
+  StringMultiple = 4,
+};
+
+struct OptionConfigItem {
+  ArgType argType;
+  std::optional<std::string> defaultValue;
+  std::optional<std::string> alias;
+  OptionConfigItem(ArgType a) : argType(a) {}
+  OptionConfigItem(ArgType a, std::string d) : argType(a), defaultValue(d) {}
+  OptionConfigItem(ArgType a, std::string d, std::string al)
+      : argType(a), defaultValue(d), alias(al) {}
+  OptionConfigItem(ArgType a, int i, std::string al) : argType(a), alias(al) {}
+};
+
+typedef std::unordered_map<std::string, OptionConfigItem> OptionConfig;
+typedef std::unordered_map<std::string, std::vector<std::string>> Options;
+
+int parseCommandLineArgs(
+    char const* usage, OptionConfig const& optionConfig, int argc, char* argv[],
+    std::unordered_map<std::string, std::vector<std::string>>& options,
+    std::vector<std::string>& args);
+
