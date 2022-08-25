1a619fa06fbb43e056a1f308c9983c7b878f6e4c
Max Neunhoeffer <max@arangodb.com>
2022-08-25 09:02:48 +0200

    Add a test for smartifier2.

---
+++ tests/test1/todo.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+../../build/smartifier2 vertices --type csv --input profiles.csv --output profiles_smart.csv --smart-graph-attribute country
+
+if ! cmp profiles_smart.csv profiles_expected.csv ; then
+    echo Error in profiles_smart.csv!
+    exit 1
+fi
+
+cp relations.csv relations_smart.csv
+../../build/smartifier2 edges --type csv --vertices profiles:profiles_smart.csv --edges relations_smart.csv:profiles:profiles
+
+if ! cmp relations_smart.csv relations_expected.csv ; then
+    echo Error in relations.csv!
+    exit 2
+fi
+
+rm profiles_smart.csv relations_smart.csv
